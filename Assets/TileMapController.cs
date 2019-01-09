64f78b7d609523117a7161fc2662348c7a81ae63
Castaire <iv.castaire@gmail.com>
2019-01-08 21:40:53 -0800

    changed scaling of tilemap

--- Assets/TileMapController.cs
+++ Assets/TileMapController.cs
@@ -16,36 +16,53 @@ public class TileMapController : MonoBehaviour
     public void startAutoGenMap(){
         Debug.Log("starting autogeneration process!");
 
+        // TESTING:
+
+
         // set variables
         tilemap = gameObject.GetComponent<Tilemap>();
+        int halfX = 19;  // in terms of cells
+        int halfY = 8;   // in terms of cells
 
-        addWall();
+        addWall(halfX, halfY);
+        
+
 
 
     }
 
     // USAGE: adds 1-depth walls to the grid within canvass
-    //        grid is centered at (0,0), +/- 13 (x), +/- 5 (y)
-    // TODO: 
-    public void addWall(){
+    // NOTE:  grid is centered at cell position (0, 0, 0)
+    public void addWall(int halfX, int halfY){
+
         // top / bottom boundary
-        for(int i = -13; i <= 13; i++){
-            tilemap.SetTile(new Vector3Int(i, -5, 0), tile);
-            tilemap.SetTile(new Vector3Int(i, 5, 0), tile);
+        for(int i = -halfX; i <= halfX; i++){
+            tilemap.SetTile(new Vector3Int(i, -halfY, 0), tile);
+            tilemap.SetTile(new Vector3Int(i, halfY, 0), tile);
         }
 
         // left / right boundary
-        for(int i = -5; i <= 5; i++){
-            tilemap.SetTile(new Vector3Int(-13, i, 0), tile);
-            tilemap.SetTile(new Vector3Int(13, i, 0), tile);
+        for(int i = -halfY; i <= halfY; i++){
+            tilemap.SetTile(new Vector3Int(-halfX, i, 0), tile);
+            tilemap.SetTile(new Vector3Int(halfX, i, 0), tile);
         }
     }
 
-    //public Vector3Int[] generateRandomPositionArray(){
-    public void generateRandomPositionArray(){
+    public Vector3Int[] generateRandomPositionArray(){
+
+        Vector3Int[] tilePositions = new Vector3Int[4];
+
+        // TODO: NOTICE ME SENPAI
 
 
-        
+        return(tilePositions);
+    }
+
+    // USAGE: prints input array of positions as tiles
+    public void arrayToTiles(Vector3Int[] arr, Tile t){
+
+
+
     }
 
 
